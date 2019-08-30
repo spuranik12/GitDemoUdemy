2f0efe3e2222a67ddebac2361494e819ec038a94
shrihari <puranikshrihari@gmail.com>
2019-08-30 11:34:55 +0530

    Committed by second person to get updated code

--- src/test/java/Mavenjava/SeleniumTest.java
+++ src/test/java/Mavenjava/SeleniumTest.java
@@ -7,6 +7,7 @@ public class SeleniumTest {
 	@Test
 	public void browserAutomation(){
 		System.out.println("browserAutomation");
+		System.out.println("Git Updated from downloaded code");
 	}
 	
 	@Test
