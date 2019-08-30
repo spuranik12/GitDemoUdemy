2c6f9153b59cee334b770f6d24153788dfb8aaf9
shrihari <puranikshrihari@gmail.com>
2019-08-30 10:40:32 +0530

    First commit

---
+++ src/test/java/Mavenjava/AppiumTest.java
@@ -0,0 +1,16 @@
+package Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void nativeAppAndroid(){
+		System.out.println("nativeAppAndroid");
+	}
+	
+	@Test
+	public void iosApp(){
+		System.out.println("iosApp");
+	}
+}
---
+++ src/test/java/Mavenjava/SeleniumTest.java
@@ -0,0 +1,16 @@
+package Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void browserAutomation(){
+		System.out.println("browserAutomation");
+	}
+	
+	@Test
+	public void elementsOnUI(){
+		System.out.println("elementsOnUI");
+	}
+}
